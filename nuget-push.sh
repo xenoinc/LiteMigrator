fefec4b6d598b4a69fe4236cc78b137b0f4b60fa
Damian Suess <djsuess@gmail.com>
2022-03-07 13:22:18 -0500

    Added notes

--- nuget-push.sh
+++ nuget-push.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Usage:
+# 0. Create an API key at https://www.nuget.org/account/apikeys and store it in ~/nuget.org.key
+# 1. Increment the package version number in source/LiteMigrator/Xeno.LiteMigrator.csproj
+# 2. Run `dotnet build source` or `dotnet pack source`  (where 'source' is the folder)
+# 3. Run this script to publish
+
+
 NUGET_PKG=$1
 
 echo "Publishing package, $NUGET_PKG"
